b0cfd18fb7a3105e5cf3d6edde3a3b288751e2ea
Igor Kadavý <xkadavy1@fi.muni.cz>
2024-12-10 21:08:22 +0100

    formating cards

--- black_jack.py
+++ black_jack.py
@@ -1,4 +1,4 @@
-import random
+from random import randrange
 
 HEARTS = 0
 DIAMONDS = 1
@@ -8,21 +8,24 @@ SPADES = 3
 class Player:
     name: str
     cards: set[tuple[int, int]]
-    bet: int = 0
+    bet: int
+    state: bool
+    result: tuple[int, int]|None = None
     def __init__(self, name: str, bet: int=0):
         self.name = name
         self.bet = bet
         self.cards = set()
+        state = True
 
 
 class BlackJack:
-    deck: dict[tuple[int, int], bool]
+    deck: list[tuple[int, int]]
     crupier_cards: set[tuple[int, int, bool]] = set()
     players: dict[str, Player]
 
 
     def __init__(self):
-        self.deck = dict([((symbol, value), False) for symbol in range(4) for value in range(13)])
+        self.deck = [((symbol, value), False) for symbol in range(4) for value in range(1, 14)]
         self.crupier_cards = set()
         self.players_cards = {}
         self.players_bets = {}
@@ -40,6 +43,48 @@ class BlackJack:
         
         self.players.pop(player_name)
         return "Player " + player_name + " was removed from the game!"
+    
+    def game_init(self) -> str:
+        return "Players who wants to participate write \"TBD bet\" after all of you are ready write \"TBD start\""
+    
+    def format_card(self, card: tuple[int, int]):
+        suits = {0: "♥", 1: "♦", 2: "♣", 3: "♠"}
+        values = {1: "A", 11: "J", 12: "Q", 13: "K"}
+
+        s, v = card
+        suit = suits.get(s, "?")
+        value = values.get(v, str(v))
+
+        return f"{value}{suit}"
+
+    def format_cards(self)-> str:
+        formated: str = "Crupier: "
+        for suit, value, hidden in self.crupier_cards:
+            if (hidden):
+                formated += "?? "
+            else:
+                formated += f"{self.format_card((suit, value))} "
+        formated += "\n"
+        for player in self.players.values():
+            formated += f"{player.name}: "
+            for card in player.cards:
+                formated += f"{self.format_card(card)} "
+            formated += "\n"
+        return formated
+        
+
+    def give_cards(self) -> str:
+        sym, val = self.deck.pop(randrange(len(self.deck)))
+        self.crupier_cards.add((sym, val, False))
+        for _ in range(2):
+            for player in self.players.values():
+                player.cards.add(self.deck.pop(randrange(len(self.deck))))
+        sym, val = self.deck.pop(randrange(len(self.deck)))
+        self.crupier_cards.add((sym, val, True))
+        return self.format_cards()
+        
+
+
     
 
 
